d7eaea6719a8fb3c5f1dda8b1c3fcf9bc723dd46
zhid0399123 <zhid0399123@gmail.com>
2024-02-18 21:46:15 +0300

    feat: demo graphql schema for product Query
    Signed-off-by: zhid0399123 <zhid0399123@gmail.com>

--- src/schemas/gql/gql-schema.js
+++ src/schemas/gql/gql-schema.js
@@ -1,8 +1,19 @@
 const { buildSchema } = require("graphql")
 
 const schema = buildSchema(`
+type Product {
+    productid: String,
+    name: String,
+    category: String,
+    description: String,
+    quantity: Int,
+    unit: String,
+    price: String,
+    date: String
+}
+
 type Query {
-    hello: String
+    product: Product
 }`)
 
 module.exports = schema
