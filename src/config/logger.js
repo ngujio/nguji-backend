d60a6c2f602c714470eb4077143f6d298080ba04
zhid0399123 <zhid0399123@gmail.com>
2024-02-23 14:22:01 +0300

    chore: get responseTime from reply.elapsedTime
    Signed-off-by: zhid0399123 <zhid0399123@gmail.com>

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,12 +1,12 @@
 module.exports = {
-  level: process.env.LOG_LEVEL || "warn",
-  transport: { target: "pino-pretty" },
+  level: process.env.LOG_LEVEL || 'warn',
+  transport: { target: 'pino-pretty' },
   timestamp: () => {
     return `,"@timestamp": "${new Date(Date.now()).toDateString()}"`
   },
   redact: {
-    censor: "********",
-    paths: ["req.headers.authorization", "req.body.password", "req.body.email"],
+    censor: '********',
+    paths: ['req.headers.authorization', 'req.body.password', 'req.body.email']
   },
   serializers: {
     req: function (request) {
@@ -15,20 +15,20 @@ module.exports = {
         method: request.method,
         url: request.url,
         routeUrl: request.routeOptions.url,
-        version: request.headers?.["accept-version"],
+        version: request.headers?.['accept-version'],
         user: request.user?.id,
         headers: request.headers,
         body: shouldLogBody ? request.body : undefined,
         hostname: request.hostname,
         remoteAddress: request.ip,
-        remotePort: request.socket?.remotePort,
+        remotePort: request.socket?.remotePort
       }
     },
     res: function (reply) {
       return {
         statusCode: reply.statusCode,
-        responseTime: reply.getResponseTime(),
+        responseTime: reply.elapsedTime
       }
-    },
-  },
+    }
+  }
 }
