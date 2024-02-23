e4e64fbb5b8bef1998c008da4e0aa6d099842008
zhid0399123 <zhid0399123@gmail.com>
2024-02-23 14:14:12 +0300

    feat(regex): removes unwanted fields from product args
    Signed-off-by: zhid0399123 <zhid0399123@gmail.com>

---
+++ src/util/regex.js
@@ -0,0 +1,4 @@
+const productRegx =
+  /supplier_email|supplier_name|supplier_mobile|expense_amount|expense_type|expense_amount|vendor_email|vendor_mobile|vendor_name|amount/
+
+module.exports = productRegx
