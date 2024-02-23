3fd3179d012669ff1959ff741a189524edb0c977
zhid0399123 <zhid0399123@gmail.com>
2024-02-23 14:20:14 +0300

    chore: enabled graphiql
    Signed-off-by: zhid0399123 <zhid0399123@gmail.com>

--- src/plugins/mercurius.js
+++ src/plugins/mercurius.js
@@ -1,11 +1,12 @@
-const mercurius = require("mercurius")
-const fp = require("fastify-plugin")
-const resolvers = require("../resolver/index")
-const gqlSchema = require("../schemas/gql/gql-schema")
+const mercurius = require('mercurius')
+const fp = require('fastify-plugin')
+const resolvers = require('../resolver/index')
+const gqlSchema = require('../schemas/gql/gql-schema')
 
 module.exports = fp(async function (fastify, opts) {
   fastify.register(mercurius, {
     schema: gqlSchema,
-    resolvers,
+    graphiql: true,
+    resolvers
   })
 })
